59854b346558a4004fbd2e8687467c2fe3f495a8
Raghava Murugudu <rag850@gmail.com>
2023-07-20 22:45:42 +0530

    Added solo play routes and controllers

---
+++ backend/paragraphs/easy.js
@@ -0,0 +1,22 @@
+module.exports = [
+	"The quick brown fox jumps over the lazy dog. Pack my box with five dozen liquor jugs. Grumpy wizards make a toxic brew for the jovial queen. Jackdaws love my big sphinx of quartz.",
+	"The waves crashed against the shore, creating a mesmerizing rhythm. The sun dipped below the horizon, painting the sky in hues of pink and orange. Seagulls soared above, calling out to each other. A gentle breeze carried the scent of salt and seaweed.",
+	"The old oak tree stood tall in the middle of the meadow, its branches reaching for the sky. A group of children played tag around it, their laughter echoing through the air. The leaves rustled in the wind, creating a soothing melody.",
+	"On a clear night, the stars twinkle like diamonds in the dark sky. The moon casts a soft glow, illuminating the world below. Crickets chirp, and owls hoot, creating a symphony of nature's sounds.",
+	"The smell of freshly baked cookies wafts through the house, inviting everyone to the kitchen. Chocolate chip, oatmeal raisin, and peanut butter cookies sit on the counter, tempting all who pass by.",
+	"In the bustling city, people hurry to their destinations, their footsteps echoing on the sidewalks.Cars honk, and buses rumble, adding to the cacophony of urban life.Skyscrapers tower overhead, touching the clouds.",
+	"The aroma of blooming flowers fills the air, signaling the arrival of spring. Cherry blossoms and tulips paint the landscape with vibrant colors. Bees buzz happily as they collect nectar from one flower to another.",
+	"The laughter of children playing in the park brings joy to everyone around.Swings creak, and slides squeak as they are used with enthusiasm.Parents sit on benches, watching their little ones explore the playground.",
+	"The sound of a gentle rain tapping on the window creates a cozy atmosphere inside the house.A cup of hot cocoa warms the hands as one settles down with a good book.",
+	"The sound of a guitar strumming fills the room, as a musician plays a soulful melody.The audience listens attentively, captivated by the music's emotional depth.",
+	"The scent of freshly cut grass fills the air as a lawnmower glides across the yard. The sun shines brightly, making the green blades glisten.",
+	"A fluffy white cat curls up on a soft cushion, purring contently.Its eyes close as it drifts off to sleep, dreaming of sunny afternoons.",
+	"The sound of waves crashing against the cliffs creates a soothing backdrop to a beach picnic.Families gather to enjoy the sun and sand, laughing and playing games.",
+	"The aroma of popcorn fills the movie theater as people settle into their seats, eager to watch the latest blockbuster.",
+	"The soft hum of a computer fills the office as employees work diligently on their tasks.The click - clack of keyboards echoes through the space.",
+	"The smell of fresh laundry fills the air as clothes dry on the line, swaying gently in the breeze.",
+	"The sound of a crackling fireplace warms the room as friends gather for a cozy evening together.",
+	"The clinking of utensils and chatter fill the dining room as a family enjoys a delicious home - cooked meal.",
+	"The chirping of birds greets the morning as the sun rises, painting the sky in beautiful colors.",
+	"The sound of a camera shutter captures the moment as a photographer snaps a stunning landscape."
+]

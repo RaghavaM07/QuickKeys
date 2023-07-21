b7802bf106d5fed3d4a2cb57da396156d290fea2
Raghava Murugudu <rag850@gmail.com>
2023-07-21 18:16:20 +0530

    Added basic MP socket endpoints

---
+++ backend/models/gameroom.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const gameroomSchema = mongoose.Schema({
+	creator: {
+		type: String,
+		required: true
+	},
+	roomId: {
+		type: String,
+		required: true
+	},
+	startBy: {
+		type: Date,
+		default: () => Date.now() + 60
+	},
+	endBy: {
+		type: Date,
+		required: true
+	},
+	para: {
+		type: String,
+		required: true
+	}
+})
+
+module.exports = mongoose.model('Gameroom', gameroomSchema)
